5bc5d5c2b96cfe91176d859c064a7ccebdee3264
Deepak Bhatt <dbhatt@kabloomcorp.com>
2018-10-22 13:13:42 +0530

    New codes bootstrap files navigation

--- react-route-data/src/header.js
+++ react-route-data/src/header.js
@@ -1,41 +1,21 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import logo from './img/logo.svg';
+//import logo from './img/logo.svg';
+// import logo from './img/logo.png';
 import './css/index.css';
 
+import Navigation from './components/navigation';
+
 class Header extends Component {
-/*   active = {
-    fontWeight: "bold",
-    color: "red"
-  }; */
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-			<div className="text-center col-lg-12 col-sm-12 col-md-12 col-xs-12">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome</h1>
-				</header>
-			</div>
-		</div>
-		<div className="row ">
-			<div className="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-				<nav>
-        		<NavLink exact to="/" className ='nav-menu'>
-						  Home Page
-						</NavLink>
-				
-						<NavLink to="/search" className ='nav-menu'>
-						  Search data
-						</NavLink>
-
-						<NavLink to="/submit" className ='nav-menu'>
-						  Save Record
-						</NavLink>
-					</nav>
-			</div>
-		</div>
+    			<div className="text-center col-lg-12 col-sm-12 col-md-12 col-xs-12">
+    				<header>
+    					  <Navigation />
+    				</header>
+    			</div>
+    		</div>
 	  </div>
     );
   }
--- react-route-data/src/index.js
+++ react-route-data/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router}  from 'react-router-dom';
 import ReactRouter from './router/router';
-
-
-
-
-
-
-ReactDOM.render(<Router>
-
+ReactDOM.render(
+  <Router>
     <ReactRouter/>
-</Router>
+  </Router>
     ,
     document.getElementById('root'));
