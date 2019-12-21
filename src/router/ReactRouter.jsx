e885e385cb14c3071a3127a0cdb3f702b84087d0
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-21 22:08:54 +0530

    refector

--- src/router/ReactRouter.jsx
+++ src/router/ReactRouter.jsx
@@ -1,35 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import App from "../components/App";
-import Search from "../components/search";
-import Submit from "../components/submit";
-import SearchRecord from "../components/searchRecord";
-import RegistrationForm from "../components/RegistrationForm";
+/** @format */
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from '../components/App';
+import Search from '../components/search';
+import Submit from '../components/submit';
+import SearchRecord from '../components/searchRecord';
+import RegistrationForm from '../components/RegistrationForm';
+
+import RegistrationFormHooks from '../components/RegistrationFormHooks';
 import SimpleForm from '../components/SimpleForm';
-import Header from "../header";
-import Footer from "../footer";
-import ListUsers from "../components/ListUsers";
-import SearchList from "../components/SearchList";
+import Header from '../header';
+import Footer from '../footer';
+import ListUsers from '../components/ListUsers';
+import SearchList from '../components/SearchList';
 
 const ReactRouter = () => {
   return (
     <Router>
       <div>
         <Header />
-        <div className="container-fluid">
-          <Route exact path="/" component={App} />
-          <Route path="/Search" component={Search} />
-          <Route path="/Submit" component={Submit} />
-          <Route path="/searchRecord" component={SearchRecord} />
-          <Route path="/register" component={RegistrationForm} />
-          <Route path="/SimpleForm" component={SimpleForm} />
-          <Route path="/ListUsers" component={ListUsers} />
-          <Route path="/searchR" component={SearchList} />
+        <div className='container-fluid'>
+          <Route exact path='/' component={App} />
+          <Route path='/Search' component={Search} />
+          <Route path='/Submit' component={Submit} />
+          <Route path='/searchRecord' component={SearchRecord} />
+          <Route path='/register' component={RegistrationForm} />
+          <Route path='/SimpleForm' component={SimpleForm} />
+          <Route path='/ListUsers' component={ListUsers} />
+          <Route path='/searchR' component={SearchList} />
+          <Route path='/registerHook' component={RegistrationFormHooks} />
         </div>
         <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default ReactRouter;
