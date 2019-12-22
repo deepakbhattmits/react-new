ac29b8f5a08bd0d2027936fc60e1ffd90b4f2f7c
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-22 08:47:53 +0530

    refector

--- src/components/ScrollApp.jsx
+++ src/components/ScrollApp.jsx
@@ -1,34 +1,37 @@
+/** @format */
+
 import React, { Component } from 'react';
 
 class ScrollButton extends Component {
-
   state = {
     intervalId: 0
   };
 
   scrollStep = () => {
-    // console.log("scroll")
+    // console.log("scroll app")
     if (window.pageYOffset === 0) {
       clearInterval(this.state.intervalId);
     }
     window.scroll(0, window.pageYOffset - this.props.scrollStepInPx); // here the props
-  }
+  };
   scrollToTop = () => {
-    let intervalId = setInterval(this.scrollStep.bind(this), this.props.delayInMs); // here the props
+    let intervalId = setInterval(
+      this.scrollStep.bind(this),
+      this.props.delayInMs
+    ); // here the props
     this.setState({ intervalId: intervalId });
-  }
+  };
   render() {
-    return <button title='Back to top' className='scroll'
-      onClick={this.scrollToTop}>
-      <i className='fa fa-angle-up'></i>
-      <span className='text-uppercase'>top</span>
-    </button>;
+    return (
+      <button title='Back to top' className='scroll' onClick={this.scrollToTop}>
+        <i className='fa fa-angle-up'></i>
+        <span className='text-uppercase'>top</span>
+      </button>
+    );
   }
 }
 
-
 class ScrollApp extends Component {
-
   // eslint-disable-next-line class-methods-use-this
   componentDidMount() {
     window.addEventListener('scroll', () => {
@@ -37,7 +40,6 @@ class ScrollApp extends Component {
       if (h > 5) {
         if (window.scrollY > 6) {
           element.classList.add('scroll');
-
         } else {
           element.classList && element.classList.remove('scroll');
         }
@@ -46,8 +48,9 @@ class ScrollApp extends Component {
   }
   render() {
     return (
-      <div id="BottomTopScroll" className="long">
-        <ScrollButton scrollStepInPx="50" delayInMs="25.66" /> {/* upper container */}
+      <div id='BottomTopScroll' className='long'>
+        <ScrollButton scrollStepInPx='50' delayInMs='25.66' />{' '}
+        {/* upper container */}
       </div>
     );
   }
