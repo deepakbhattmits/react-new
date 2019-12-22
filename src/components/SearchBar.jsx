e29eaa6e49f140fea0175713d74fb8cd1525b9be
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-22 08:48:03 +0530

    refector

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,29 @@
+/** @format */
+
 import React from 'react';
 
-
-const SearchBar = ( props ) => {
-
-    return (
-        <div>
-            <form className="ui form " onSubmit={ props.getRecipe } style={{ margin: '10px' }}>
-                <div className="ui search focus">
-                        <div className="ui icon input">
-                            <input  
-                                className='prompt'
-                                type='text'
-                                name='recipeName'
-                                placeholder='search here...'
-                                autoComplete='off'
-                            />
-                            <i className="search icon" areahidden='true'/>
-                        </div>
-                    </div>
-            </form>
+const SearchBar = ({ getRecipe }) => {
+  return (
+    <div>
+      <form
+        className='ui form '
+        onSubmit={getRecipe}
+        style={{ margin: '10px' }}
+      >
+        <div className='ui search focus'>
+          <div className='ui icon input'>
+            <input
+              className='prompt'
+              type='text'
+              name='recipeName'
+              placeholder='search here...'
+              autoComplete='off'
+            />
+            <i className='search icon' areahidden='true' />
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 export default SearchBar;
