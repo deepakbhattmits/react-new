806e794011b73c32f83a44038715fc61a9555fd5
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-31 21:26:08 +0530

    refector

--- src/components/SearchRecipe.jsx
+++ src/components/SearchRecipe.jsx
@@ -8,73 +8,73 @@ const API_KEY = `209e1a0d02a7b3e2c423e14995ac0a55`;
 // const API_URL = `https://www.food2fork.com/api/search?key=${API_KEY}&q=${}&count=5`;
 
 class SearchRecipe extends Component {
-  state = {
-    data: [],
-    movies: [],
-    selectedOption: '-- Select Option --'
-  };
+	state = {
+		data: [],
+		movies: [],
+		selectedOption: '-- Select Option --'
+	};
 
-  getRecipe = e => {
-    const term = e.target.elements.recipeName.value;
-    e.preventDefault();
-    // console.log('term ', term);
-    fetch(
-      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${term}&count=5`,
-      {
-        mode: 'no-cors',
-        credentials: 'include'
-      }
-    )
-      .then(response => response.json())
+	getRecipe = e => {
+		const term = e.target.elements.recipeName.value;
+		e.preventDefault();
+		// console.log('term ', term);
+		fetch(
+			`https://www.food2fork.com/api/search?key=${API_KEY}&q=${term}&count=5`,
+			{
+				mode: 'no-cors',
+				credentials: 'include'
+			}
+		)
+			.then(response => response.json())
 
-      .then(json => {
-        // console.log('test : ',json, "TEST : ",json.data);
-        this.setState({ data: json });
-        console.log(this.state.data);
-      })
-      .catch(function(error) {
-        // console.log("error:", error);
-      });
-  };
-  getOption = e => {
-    console.log('selected option : ', e.target.value);
-    this.setState({ selectedOption: e.target.option });
-    fetch(
-      `https://cors-anywhere.herokuapp.com/http://starlord.hackerearth.com/movies`
-    )
-      .then(response => response.json())
+			.then(json => {
+				// console.log('test : ',json, "TEST : ",json.data);
+				this.setState({ data: json });
+				console.log(this.state.data);
+			})
+			.catch(function(error) {
+				// console.log("error:", error);
+			});
+	};
+	getOption = e => {
+		console.log('selected option : ', e.target.value);
+		this.setState({ selectedOption: e.target.option });
+		fetch(
+			`https://cors-anywhere.herokuapp.com/http://starlord.hackerearth.com/movies`
+		)
+			.then(response => response.json())
 
-      .then(json => {
-        // console.log('pre test : ',json, "TEST : ",json.data);
-        this.setState({ movies: json });
-        console.log('loaded state', this.state.movies);
-      })
-      .catch(function(error) {
-        // console.log("error:", error);
-      });
-    // this.state.data.recipes.filter((el,i,self) => { return el['title'] === (e.target.value) ? this.setState({data: self }): this.setState({ data: self })} );
-  };
-  render() {
-    return (
-      <div className='ui grid'>
-        <div className='four wide column'>
-          <div className='ui search'>
-            <SearchBar getRecipe={this.getRecipe} />
-          </div>
+			.then(json => {
+				// console.log('pre test : ',json, "TEST : ",json.data);
+				this.setState({ movies: json });
+				console.log('loaded state', this.state.movies);
+			})
+			.catch(function(error) {
+				// console.log("error:", error);
+			});
+		// this.state.data.recipes.filter((el,i,self) => { return el['title'] === (e.target.value) ? this.setState({data: self }): this.setState({ data: self })} );
+	};
+	render() {
+		return (
+			<div className='ui grid'>
+				<div className='four wide column'>
+					<div className='ui search'>
+						<SearchBar getRecipe={this.getRecipe} />
+					</div>
 
-          <div>
-            <SelectBar
-              movies={this.state.movies}
-              getOption={this.getOption}
-              value={this.state.selectedOption}
-            />
-          </div>
-        </div>
-        <div className='twelve wide column'>
-          <SearchResult data={this.state.data} />
-        </div>
-      </div>
-    );
-  }
+					<div>
+						<SelectBar
+							movies={this.state.movies}
+							getOption={this.getOption}
+							value={this.state.selectedOption}
+						/>
+					</div>
+				</div>
+				<div className='twelve wide column'>
+					<SearchResult data={this.state.data} />
+				</div>
+			</div>
+		);
+	}
 }
 export default SearchRecipe;
