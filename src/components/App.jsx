f2174c8ec1ca1f52c0b7ea6590ccfb2b27b6f7d0
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-03 20:36:12 +0530

    refector

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ class App extends Component {
 	};
 
 	renderLoadButton = () => {
-		// console.log('TEST:', this.props);
+		// console.log('TEST :', this.props);
 		const { isSignedIn } = this.props.auth;
 
 		if (isSignedIn) {
