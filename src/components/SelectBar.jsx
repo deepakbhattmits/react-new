4b02884e90a1d5e999b4acd1ae3d9235a8f3d3f7
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-08 22:08:34 +0530

    added refector

--- src/components/SelectBar.jsx
+++ src/components/SelectBar.jsx
@@ -3,27 +3,26 @@
 import React from 'react';
 
 const SelectBar = props => {
-  // console.log(`NEW SELECT BAR: ${props.getOption}`);
-  return (
-    <div>
-      <select
-        className='ui dropdown'
-        onChange={props.getOption}
-        value={props.selectedOption}
-      >
-        <option value=''> -- Select Choice -- </option>
-        {/* { console.log(this.state.data) } */}
-        {props.movies &&
-          props.movies.map((el, i) => {
-            // console.log(`test : ${ i }`);
-            return (
-              <option key={i} value={el['country']}>
-                {el['country']}
-              </option>
-            );
-          })}
-      </select>
-    </div>
-  );
+	console.log(`NEW SELECT BAR: ${props.getOption}`);
+	return (
+		<div>
+			<select
+				className='ui dropdown'
+				onChange={props.getOption}
+				value={props.selectedOption}>
+				<option value=''> -- Select Choice -- </option>
+				{/* { console.log(this.state.data) } */}
+				{props.movies &&
+					props.movies.map((el, i) => {
+						// console.log(`test : ${ i }`);
+						return (
+							<option key={i} value={el['country']}>
+								{el['country']}
+							</option>
+						);
+					})}
+			</select>
+		</div>
+	);
 };
 export default SelectBar;
