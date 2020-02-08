866c4899016f2e2fed1aa3cfe58ec11dc8ad61da
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-08 22:05:40 +0530

    added refector

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+/** @format */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import './assets/css/main.css';
+import './assets/css/_main.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import ReactRouter from './router/ReactRouter';
@@ -12,12 +15,13 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware(reduxThunk))
+	reducers,
+	composeEnhancers(applyMiddleware(reduxThunk))
 );
-const rootElement = document.querySelector('#root')
+const rootElement = document.querySelector('#root');
 ReactDOM.render(
-    <Provider store={store}>
-        <ReactRouter />
-    </Provider>
-    , rootElement);
+	<Provider store={store}>
+		<ReactRouter />
+	</Provider>,
+	rootElement
+);
