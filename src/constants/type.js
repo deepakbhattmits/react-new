b9bf7ac73ea841fa38f57af075964a04f634b4e1
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-09 08:59:38 +0530

    refector

--- src/constants/type.js
+++ src/constants/type.js
@@ -1,4 +1,6 @@
-export const SIGN_IN = 'SIGN_IN';
+/** @format */
+
+export const SIGN_IN = `SIGN_IN`;
 export const SIGN_OUT = 'SIGN_OUT';
 export const CREATE_USER = 'CREATE_USER';
-export const LIST_USERS = 'LIST_USERS'; 
+export const LIST_USERS = 'LIST_USERS';
