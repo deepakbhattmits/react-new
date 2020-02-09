f8057cd4af90e44823eb1ee03c359acda1eeef7f
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-09 08:59:29 +0530

    refector

--- src/header.js
+++ src/header.js
@@ -1,18 +1,20 @@
-import React from "react";
-import Navigation from './components/navigation'
+/** @format */
+
+import React from 'react';
+import Navigation from './components/navigation';
 const Header = () => {
-  // console.log('test : ')
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="text-center col-lg-12 col-sm-12 col-md-12 col-xs-12">
-          <header className="App-header">
-            <Navigation />
-          </header>
-        </div>
-      </div>
-    </div>
-  );
-}
+	console.log('test : ');
+	return (
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='text-center col-lg-12 col-sm-12 col-md-12 col-xs-12'>
+					<header className='App-header'>
+						<Navigation />
+					</header>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Header;
