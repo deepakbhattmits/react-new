6b0e9e303b1c97351541776f1b3c4c173b1341e7
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-04 20:28:26 +0530

    refector

--- src/footer.js
+++ src/footer.js
@@ -1,22 +1,23 @@
-import React from "react";
+/** @format */
+
+import React from 'react';
 import ScrollApp from './components/ScrollApp';
-const Footer = () => { 
-	// console.log('TEST : ')
+const Footer = () => {
+	console.log('TEST : ');
 
 	return (
-		<div className="container-fluid">
-			<div className="row">
-				<div className="text-center col-lg-12 col-sm-12 col-md-12 col-xs-12">
-					<footer className="App-footer">
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='text-center col-lg-12 col-sm-12 col-md-12 col-xs-12'>
+					<footer className='App-footer'>
 						<ScrollApp />
 					</footer>
 				</div>
 			</div>
-			<div className="row ">
-				<div className="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-				</div>
+			<div className='row '>
+				<div className='col-lg-12 col-sm-12 col-md-12 col-xs-12'></div>
 			</div>
 		</div>
 	);
-}
+};
 export default Footer;
