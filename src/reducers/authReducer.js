ec8c8b4efe03ad582d42fe2cc316f8568622434f
deepak bhatt <deepakbhattmits@gmail.com>
2020-05-09 10:26:31 +0530

    refactor

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,12 +3,12 @@
 import { SIGN_IN, SIGN_OUT } from '../constants/type';
 const INITIAL_STATE = {
 	isSignedIn: null,
-	userId: null
+	userId: null,
 };
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case SIGN_IN:
-			console.log('test :', action.payload);
+			// console.log('test :', action.payload);
 			return { ...state, isSignedIn: true, userId: action.payload };
 		case SIGN_OUT:
 			return { ...state, isSignedIn: false, userId: null };
