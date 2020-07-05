6a5f90b1fcda85366a0ff5b302fd146061ee7f2d
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-05 15:28:56 +0530

    refactor

--- src/components/reusable/Save.jsx
+++ src/components/reusable/Save.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const Save = ({ isValid, type, children }) => {
-	// console.log(isValid)
 	return (
 		<div>
 			<Button type={type} disabled={!isValid}>
