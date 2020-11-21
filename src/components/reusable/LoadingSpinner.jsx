0ab6f705ec9479fea71bd7bcecec56b5d0bfb786
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:01:55 +0530

    Update LoadingSpinner.jsx

--- src/components/reusable/LoadingSpinner.jsx
+++ src/components/reusable/LoadingSpinner.jsx
@@ -2,15 +2,13 @@
 
 import React from 'react';
 
-const LoadingSpinner = () => {
-	return (
-		<div className='ui inverted segment'>
-			<div className='ui active inverted loader'></div>
-			<br />
-			<br />
-			<br />
-			<br />
-		</div>
-	);
-};
+const LoadingSpinner = () => (
+	<div className='ui inverted segment'>
+		<div className='ui active inverted loader'></div>
+		<br />
+		<br />
+		<br />
+		<br />
+	</div>
+);
 export default LoadingSpinner;
