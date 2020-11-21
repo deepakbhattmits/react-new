26708e1759bbd63688d9ea805448dd1c84a006ab
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:01:18 +0530

    Update TextInput.jsx

--- src/components/reusable/TextInput.jsx
+++ src/components/reusable/TextInput.jsx
@@ -3,23 +3,21 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
-const TextInput = ({ placeholder, value, handleChange, error }) => {
-	return (
-		<Form.Group>
-			<Form.Label>IP address</Form.Label>
-			<Form.Control
-				className={`input`}
-				onChange={handleChange}
-				type='text'
-				autoComplete='off'
-				placeholder={placeholder}
-				value={value}
-			/>
+const TextInput = ({ placeholder, value, handleChange, error }) => (
+	<Form.Group>
+		<Form.Label>IP address</Form.Label>
+		<Form.Control
+			className={`input`}
+			onChange={handleChange}
+			type='text'
+			autoComplete='off'
+			placeholder={placeholder}
+			value={value}
+		/>
 
-			<Form.Text className='invalid-feedback show' style={{ display: 'block' }}>
-				{error}
-			</Form.Text>
-		</Form.Group>
-	);
-};
+		<Form.Text className='invalid-feedback show' style={{ display: 'block' }}>
+			{error}
+		</Form.Text>
+	</Form.Group>
+);
 export default TextInput;
