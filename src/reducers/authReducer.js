b24ae3734dcc98c071e84b5882a56941912d4ca3
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:01:36 +0530

    Update authReducer.js

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case SIGN_IN:
-			// console.log('test :', action.payload);
+			// console.log('test : ', action.payload);
 			return { ...state, isSignedIn: true, userId: action.payload };
 		case SIGN_OUT:
 			return { ...state, isSignedIn: false, userId: null };
