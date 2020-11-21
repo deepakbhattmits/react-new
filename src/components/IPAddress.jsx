ec6643ecd0a0e134f5793a8e154b753ffa8fee93
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:00:27 +0530

    Update IPAddress.jsx

--- src/components/IPAddress.jsx
+++ src/components/IPAddress.jsx
@@ -65,6 +65,7 @@ const IPAddress = () => {
 				selectedOption={selectedOption}
 				handleChange={handleChange}
 			/>
+
 			{selectedOption !== 'Select Option' ? (
 				<>
 					<div className='input-wrapper'>
