86c54509c5445b6d4ac2e24bdcb92a7a3ad03666
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:00:13 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,23 @@
+/** @format */
+
 import { SIGN_IN, SIGN_OUT, LIST_USERS } from '../constants/type';
 import users from '../apis/users';
 export const createUser = (formValues) => async (dispatch) => {
-    users.post('/users', formValues);
+	users.post('/users', formValues);
 };
 export const listUser = () => async (dispatch) => {
-    const response = await users.get('/users');
-    console.log('test : ',response.data)
-    dispatch({ type: LIST_USERS, payload: response.data });
+	const response = await users.get('/users');
+	// console.log('test : ',response.data)
+	dispatch({ type: LIST_USERS, payload: response.data });
 };
 export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
-}
+	return {
+		type: SIGN_IN,
+		payload: userId,
+	};
+};
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
-}
-
-
+	return {
+		type: SIGN_OUT,
+	};
+};
