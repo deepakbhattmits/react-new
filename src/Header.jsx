49f91d20a1bbc8871526d12a6f57a45742f73dea
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-22 00:01:34 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 const Header = () => {
-	// console.log('test : ');
+	// console.log('test : >');
 	return (
 		<div className='container-fluid'>
 			<div className='row'>
